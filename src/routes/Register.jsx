80a87c304b668cc4a3da3f119f977c4754b76667
Iker-Romero <ikersoftdev@gmail.com>
2022-12-07 16:12:03 +0100

    Register fixed

--- src/routes/Register.jsx
+++ src/routes/Register.jsx
@@ -10,37 +10,31 @@ import {
   InputRightElement,
 } from '@chakra-ui/react';
 import { useState } from 'react';
-import React from 'react';
+import { useForm } from 'react-hook-form';
+import { useNavigate } from 'react-router-dom';
+
+import { API } from '../services/API.js';
 
 const Register = () => {
-  const [show, setShow] = useState(false);
-  const [username, setUsername] = useState('');
-  const [gmail, setGmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [image, setImage] = useState('');
-  const handleClick = () => setShow(!show);
-  const handleUsername = (e) => {
-    setUsername(e.target.value);
-  };
-  const handleGmail = (e) => {
-    setGmail(e.target.value);
-  };
-  const handlePassword = (e) => {
-    setPassword(e.target.value);
-  };
-  const handleImage = (e) => {
-    setImage(e.target.value);
-  };
+  const [showPassword, setShowPassword] = useState(false);
+  const { register, handleSubmit } = useForm();
+  const navigate = useNavigate();
 
-  const createUser = {
-    nickname: username,
-    gmail: gmail,
-    password: password,
-    image: image,
-  };
+  const submitForm = (data) => {
+    const formData = new FormData();
 
-  const handleSubmit = () => {
-    console.log(createUser);
+    console.log('data', data);
+    const { username, gmail, password, image } = data;
+    formData.append('username', username);
+    formData.append('gmail', gmail);
+    formData.append('password', password);
+    if (image.length !== 0) formData.append('image', image[0]);
+    console.log(image[0]);
+    console.log('formData', Object.entries(formData));
+    API.post('users/register', formData).then((res) => {
+      console.log('Response', res);
+      res && navigate('/login');
+    });
   };
 
   return (
@@ -56,57 +50,67 @@ const Register = () => {
         src="https://res.cloudinary.com/dlqo06xcs/image/upload/v1670349592/LOGO_gbbhqo.png"
         alt="E-Tactic"
       />
-      <FormControl>
-        <InputGroup m="10px">
-          <InputLeftElement pointerEvents="none">
-            <AtSignIcon color="gray.300" />
-          </InputLeftElement>
-          <Input
-            type="text"
-            placeholder="Username"
-            value={username}
-            onChange={handleUsername}
-          />
-        </InputGroup>
-        <InputGroup m="10px">
-          <InputLeftElement pointerEvents="none">
-            <AtSignIcon color="gray.300" />
-          </InputLeftElement>
-          <Input type="text" placeholder="Gmail" value={gmail} onChange={handleGmail} />
-        </InputGroup>
-        <InputGroup m="10px" size="md">
-          <InputLeftElement pointerEvents="none">
-            <UnlockIcon color="gray.300" />
-          </InputLeftElement>
-          <Input
-            pr="4.5rem"
-            type={show ? 'text' : 'password'}
-            placeholder="Password"
-            value={password}
-            onChange={handlePassword}
-          />
-          <InputRightElement width="4.5rem">
-            <Button h="1.75rem" size="sm" onClick={handleClick}>
-              {show ? 'Hide' : 'Show'}
-            </Button>
-          </InputRightElement>
-        </InputGroup>
-        <InputGroup m="10px">
-          <InputLeftElement pointerEvents="none">
-            <AtSignIcon color="gray.300" />
-          </InputLeftElement>
-          <Input
-            type="file"
-            placeholder="Avatar picture"
-            value={image}
-            onChange={handleImage}
-          />
-        </InputGroup>
-
-        <Button bg="#C2145A" color="#FFFFFF" variant="solid" onClick={handleSubmit}>
-          SUBMIT
-        </Button>
-      </FormControl>
+      <form onSubmit={handleSubmit(submitForm)}>
+        <FormControl>
+          <InputGroup m="10px">
+            <InputLeftElement pointerEvents="none">
+              <AtSignIcon color="gray.300" />
+            </InputLeftElement>
+            <Input
+              type="text"
+              id="username"
+              name="username"
+              {...register('username')}
+              placeholder="Username"
+            />
+          </InputGroup>
+          <InputGroup m="10px">
+            <InputLeftElement pointerEvents="none">
+              <AtSignIcon color="gray.300" />
+            </InputLeftElement>
+            <Input
+              type="text"
+              id="gmail"
+              name="gmail"
+              {...register('gmail')}
+              placeholder="Gmail"
+            />
+          </InputGroup>
+          <InputGroup m="10px" size="md">
+            <InputLeftElement pointerEvents="none">
+              <UnlockIcon color="gray.300" />
+            </InputLeftElement>
+            <Input
+              type={showPassword ? 'text' : 'password'}
+              id="password"
+              name="password"
+              {...register('password')}
+              placeholder="Password"
+              pr="4.5rem"
+            />
+            <InputRightElement width="4.5rem">
+              <Button h="1.75rem" size="sm" onClick={() => setShowPassword(true)}>
+                {showPassword ? 'Hide' : 'Show'}
+              </Button>
+            </InputRightElement>
+          </InputGroup>
+          <InputGroup m="10px">
+            <InputLeftElement pointerEvents="none">
+              <AtSignIcon color="gray.300" />
+            </InputLeftElement>
+            <Input
+              type="file"
+              id="image"
+              name="image"
+              {...register('image')}
+              placeholder="Avatar image"
+            />
+          </InputGroup>
+          <Button type="submit" bg="#C2145A" color="#FFFFFF" variant="solid">
+            SUBMIT
+          </Button>
+        </FormControl>
+      </form>
     </Box>
   );
 };
