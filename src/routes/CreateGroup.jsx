907e44d061f1bd3ceef914aca89c7d94ffed4094
Iker-Romero <ikersoftdev@gmail.com>
2022-12-08 19:58:18 +0100

    React-Hook-Form and Chakra incompatibility fixed

--- src/routes/CreateGroup.jsx
+++ src/routes/CreateGroup.jsx
@@ -1,7 +1,9 @@
-import { Box, FormControl, FormLabel, Radio, RadioGroup } from '@chakra-ui/react';
+import { Box, Button, FormControl, FormLabel, Radio, RadioGroup } from '@chakra-ui/react';
 import { useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
 
 import RadioCard from '../components/RadioCard';
+import { API } from '../services/API.js';
 import theme from './../theme';
 
 const CreateGroup = () => {
@@ -26,47 +28,89 @@ const CreateGroup = () => {
   ];
 
   const [selectedGame, setSelectedGame] = useState(null);
+  const { register, handleSubmit, control } = useForm();
+
+  const submitForm = (data) => {
+    const { game, competition } = data;
+    const formData = new FormData();
+
+    formData.append('game', game);
+    formData.append('competition', competition);
+
+    API.post('competitions', formData).then((res) => console.log('Response', res));
+  };
 
   return (
     <Box h="100vh" w="100%" bg={theme.dark.background} padding="1rem">
-      <FormControl>
-        <FormLabel htmlFor="game" color={theme.dark.primary}>
-          Select a game:
-        </FormLabel>
-        <RadioGroup name="game" display="flex" flexWrap="wrap" gap="1rem">
-          {games.map((game) => {
-            const { name, isDisabled } = game;
-            const handleClick = () => setSelectedGame(game);
-            return (
-              <RadioCard
-                key={name}
-                value={name}
-                isDisabled={isDisabled}
-                onClick={handleClick}
+      <form onSubmit={handleSubmit(submitForm)}>
+        <FormControl>
+          <FormLabel htmlFor="game" color={theme.dark.primary}>
+            Select a game:
+          </FormLabel>
+          <Controller
+            render={() => (
+              <RadioGroup
+                ref={register()}
+                name="game"
+                display="flex"
+                flexWrap="wrap"
+                gap="1rem"
               >
-                {name}
-              </RadioCard>
-            );
-          })}
-        </RadioGroup>
-        {selectedGame && (
-          <>
-            <FormLabel htmlFor="competition" color={theme.dark.primary}>
-              Competition:
-            </FormLabel>
-            <RadioGroup name="competition">
-              {selectedGame.competitions?.map((competition) => {
-                const { name, isDisabled } = competition;
-                return (
-                  <Radio key={name} isDisabled={isDisabled}>
-                    {name}
-                  </Radio>
-                );
-              })}
-            </RadioGroup>
-          </>
-        )}
-      </FormControl>
+                {games.map((game) => {
+                  const { name, isDisabled } = game;
+                  const handleClick = () => setSelectedGame(game);
+                  return (
+                    <RadioCard
+                      key={name}
+                      value={name}
+                      isDisabled={isDisabled}
+                      onClick={handleClick}
+                    >
+                      {name}
+                    </RadioCard>
+                  );
+                })}
+              </RadioGroup>
+            )}
+            name="game"
+            control={control}
+          />
+
+          {selectedGame && (
+            <>
+              <FormLabel htmlFor="competition" color={theme.dark.primary}>
+                Competition:
+              </FormLabel>
+              <Controller
+                render={() => (
+                  <RadioGroup name="competition">
+                    {selectedGame.competitions?.map((competition) => {
+                      const { name, isDisabled } = competition;
+                      return (
+                        <Radio key={name} isDisabled={isDisabled}>
+                          {name}
+                        </Radio>
+                      );
+                    })}
+                  </RadioGroup>
+                )}
+                name="competition"
+                control={control}
+              />
+            </>
+          )}
+          <Button
+            type="submit"
+            bg={theme.dark.accent3}
+            color="#FFFFFF"
+            variant="solid"
+            marginTop="1rem"
+            width="max-content"
+          >
+            Create
+          </Button>
+        </FormControl>
+      </form>
     </Box>
   );
 };
