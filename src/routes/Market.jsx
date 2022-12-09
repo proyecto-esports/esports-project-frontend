99ec977f9a3fc027a42418a006dd977322e06e27
Jesus Santos Lopez <83368883+Chule79@users.noreply.github.com>
2022-12-09 17:52:14 +0100

    create bids theme (#23)

--- src/routes/Market.jsx
+++ src/routes/Market.jsx
@@ -1,7 +1,23 @@
+import { Stack } from '@chakra-ui/react';
+
 import DatosCartaMercado from '../components/DatosCartaMercado';
+import theme from '../theme';
 
 const Market = () => {
-  return <DatosCartaMercado />;
+  return (
+    <Stack
+      w="100vw"
+      h="100vh"
+      alignItems="center"
+      display="flex"
+      bg={theme.dark.background}
+      gap="1rem"
+      padding="1rem 0rem"
+      overflow="scroll"
+    >
+      <DatosCartaMercado />
+    </Stack>
+  );
 };
 
 export default Market;
--- src/theme.jsx
+++ src/theme.jsx
@@ -4,6 +4,8 @@ const theme = {
     accent1: '#57f7E0',
     accent2: '#22BBFF',
     accent3: '#C400FF',
+    stas: '#FF3B4B',
+    bottons: '#EBE8E0',
     background: '#101221',
     popUpBackground: 'rgba(6, 44, 68, 0.66)',
   },
