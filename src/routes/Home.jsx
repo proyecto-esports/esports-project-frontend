f8dbb0d1a8c0987edd0e6dc2c7be4cd45017e795
Lauris21 <112576133+Lauris21@users.noreply.github.com>
2022-12-10 10:41:08 +0100

    NavBar (#26)

--- src/routes/Home.jsx
+++ src/routes/Home.jsx
@@ -1,9 +1,11 @@
+import NavBar from '../components/NavBar';
 import TabBar from '../components/TabBar';
 
 const Home = () => {
   return (
     <>
       <h1>Home</h1>
+      <NavBar />
       <TabBar />
     </>
   );
