d3592836c27eb0faf24bc04a7a2c4486333efbc4
Chule79 <jsantoslopez99@gmail.com>
2022-12-17 19:02:02 +0100

    Fixed: user and modal invite

---
+++ src/components/InviteGroupModal.jsx
@@ -0,0 +1,90 @@
+import {
+  Button,
+  /* Editable,
+  EditableInput,
+  EditablePreview, */
+  Flex,
+  Input,
+  Modal,
+  ModalBody,
+  ModalCloseButton,
+  ModalContent,
+  ModalFooter,
+  ModalHeader,
+  ModalOverlay,
+} from '@chakra-ui/react';
+import { useClipboard } from '@chakra-ui/react';
+import { useDisclosure } from '@chakra-ui/react';
+import React from 'react';
+import { useEffect } from 'react';
+
+import { useAuth } from '../hooks/AuthContext';
+import { API } from '../services/Api';
+import theme from '../theme';
+const InviteGroupModal = () => {
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const { onCopy, value, setValue, hasCopied } = useClipboard('');
+  const { user } = useAuth();
+  const initialRef = React.useRef(null);
+  const finalRef = React.useRef(null);
+  let group = { competition: user.competition._id };
+  console.log(user._id);
+  useEffect(() => {
+    API.get(`users/generateinvite/${user._id}`, group).then((res) => {
+      console.log(res);
+    });
+  }, [isOpen]);
+
+  return (
+    <>
+      <Button
+        onClick={onOpen}
+        bg={theme.dark.accent3}
+        color="#FFFFFF"
+        variant="solid"
+        marginTop="1rem"
+        width="max-content"
+      >
+        Invite Friends
+      </Button>
+
+      <Modal
+        initialFocusRef={initialRef}
+        finalFocusRef={finalRef}
+        isOpen={isOpen}
+        onClose={onClose}
+      >
+        <ModalOverlay />
+        <ModalContent>
+          <ModalHeader>Invite Rivals</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody pb={6}>
+            <Flex mb={2}>
+              <Input
+                value={value}
+                onChange={(e) => {
+                  setValue(e.target.value);
+                }}
+                mr={2}
+              />
+              <Button onClick={onCopy}>{hasCopied ? 'Copied!' : 'Copy'}</Button>
+            </Flex>
+            {/* <Editable placeholder="Paste here">
+              <EditablePreview width="100%" />
+              <EditableInput />
+            </Editable> */}
+          </ModalBody>
+
+          <ModalFooter>
+            <Button colorScheme="blue" mr={3}>
+              Save
+            </Button>
+            <Button onClick={onClose}>Cancel</Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
+    </>
+  );
+};
+
+export default InviteGroupModal;
